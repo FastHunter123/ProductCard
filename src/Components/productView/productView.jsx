d0bd063cbba81502a2566a16ea3a6ff4e6e01e10
FastHunter123 <le8edevstepan12@yandex.ru>
2024-06-28 13:48:14 +0300

    modified:   public/index.html 	modified:   src/Components/decription/description.module.css 	modified:   src/Components/deliveryButton/deliveryButton.module.css 	modified:   src/Components/productView/productView.jsx 	modified:   src/Components/productView/productView.module.css 	new file:   src/assets/Jura-VariableFont_wght.ttf 	new file:   src/image/pngimg.com - laptop_PNG101815.png 	modified:   src/index.css

--- src/Components/productView/productView.jsx
+++ src/Components/productView/productView.jsx
@@ -5,7 +5,7 @@ const ProductView = () => {
     return (
         <div>
             <div className={s.productImage}>
-                <img src='https://imgpng.ru/d/laptop_PNG101815.png' />
+                <img src='https://imgpng.ru/d/laptop_PNG101815.png' alt="ТУТ ДОЛЖЕН БЫТЬ НОУТБУК" />
             </div>
             <div className={s.quickView}>
                 <h3>Быстрый просмотр</h3>
